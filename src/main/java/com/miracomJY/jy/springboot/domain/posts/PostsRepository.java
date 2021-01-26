dcf01458a35e46f5f16da518330b56942e59d325
kjy93913 <kjy93913@github.com>
2021-01-26 19:57:56 +0900

    commit3

---
+++ src/main/java/com/miracomJY/jy/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,7 @@
+package com.miracomJY.jy.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts,Long> {
+
+}
