a207135e779ba41cb12a2588cedf23e5b4dd04e2
kjy93913 <kjy93913@github.com>
2021-01-27 17:15:56 +0900

    20210127_17:15

---
+++ src/main/java/com/miracomJY/jy/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,22 @@
+package com.miracomJY.jy.springboot.web.dto;
+
+
+import com.miracomJY.jy.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id =entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author= entity.getAuthor();
+
+    }
+}
