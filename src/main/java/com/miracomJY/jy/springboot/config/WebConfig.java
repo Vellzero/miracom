9916463d36cda15fcd49f63fbb630a72c6bee5fd
kjy93913 <kjy93913@github.com>
2021-02-02 10:20:55 +0900

    20210202_10:20_5장

---
+++ src/main/java/com/miracomJY/jy/springboot/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.miracomJY.jy.springboot.config;
+
+import com.miracomJY.jy.springboot.config.auth.LoginUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(loginUserArgumentResolver);
+    }
+}
